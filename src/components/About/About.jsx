7420fe89f4625a9b38276c562ad0d0003b423453
Git And Github <krjabhijeet@gmail.com>
2026-09-15 21:56:26 +0530

    change dp

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 import Tilt from 'react-parallax-tilt';
-import profileImage from '../../assets/Abhii.png';
+import profileImage from '../../assets/dp.png';
 
 const About = () => {
   return (
